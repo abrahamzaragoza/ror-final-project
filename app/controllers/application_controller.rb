0315d6cfb9feefc3ab4d79112600340a9a32d1f9
abrahamzaragoza <abraham.zaragoza@enroutesystems.com>
2022-10-05 18:16:59 -0500

    Add security updates checkbox at edit user form, Add user presenter, Update user profile with presenter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:first_name, :last_name, :email, :password, :authorization_tier)
+      u.permit(:first_name, :last_name, :email, :password, :authorization_tier, :security_updates)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:first_name, :last_name, :password, :current_password)
+      u.permit(:first_name, :last_name, :password, :current_password, :security_updates)
     end
   end
 end
