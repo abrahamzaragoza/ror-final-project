8a5945bcb9e06da843ca63a61870b0d1ecefe65a
abrahamzaragoza <abraham.zaragoza@enroutesystems.com>
2022-10-07 22:33:02 -0500

    Add stripe_product_id to plan, Add method to create stripe plan

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -35,4 +35,26 @@ class StripeService
                             customer: stripe_customer.id,
                             description: "Charge by TeamTask - #{plan.plan_name} for $#{plan.price}" })
   end
+
+  def find_or_create_product(plan)
+    if plan.stripe_product_id.present?
+      stripe_product = Stripe::Product.retrieve(plan.stripe_product_id)
+    else
+      stripe_product = Stripe::Product.create({ description: "#{plan.plan_name} for up to #{plan.plan_members} members",
+                                                name: plan.plan_name,
+                                                default_price_data: { currency: plan.price_currency,
+                                                                      unit_amount: plan.price_cents,
+                                                                      recurring: { interval_count: plan.plan_duration,
+                                                                                    interval: 'month' } } })
+      plan.update(stripe_product_id: stripe_product.id)
+    end
+    stripe_product
+  end
+
+  def create_subscription(user, plan)
+    stripe_plan = Stripe::Subscription.retrieve(plan.stripe_product_id)
+    Stripe::Subscription.create({ customer: user.stripe_id,
+                                  items: [{ price: stripe_plan.default_price }],
+                                  trial_end: Time.now.to_i + 15.days })
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_06_232738) do
+ActiveRecord::Schema.define(version: 2022_10_08_023712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2022_10_06_232738) do
     t.integer "plan_duration"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "stripe_product_id"
   end
 
   create_table "task_histories", force: :cascade do |t|
